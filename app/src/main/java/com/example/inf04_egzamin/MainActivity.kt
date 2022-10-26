e577a30f0a82bfc5a8d15a1484c319892e8fa5af
Kwiatkowska-W <wikikwiat2004@gmail.com>
2022-10-26 10:14:42 +0200

    sprawdzenie zgodności haseł oraz gdy sa poprawne wpisyanie maila w welcome

--- app/src/main/java/com/example/inf04_egzamin/MainActivity.kt
+++ app/src/main/java/com/example/inf04_egzamin/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.inf04_egzamin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            if (findViewById<EditText>(R.id.haslo1) == findViewById<EditText>(R.id.haslo2)) {
+                findViewById<TextView>(R.id.Welcome).text =
+                    "Witaj " + findViewById<TextView>(R.id.mail).text;
+            } else findViewById<TextView>(R.id.Welcome).text = "hasla sa rozne";
+        }
     }
 }
